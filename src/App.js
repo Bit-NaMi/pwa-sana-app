e8b48c3ea0e97fc2e43681aa642eac2859c8e8ec
Minna P <60325864+Bit-NaMi@users.noreply.github.com>
2020-11-18 21:17:34 +0200

    Sisältö vaihtui omaksi

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React from 'react';
+//import React, { Component } from 'react';
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import NavBar from './Navbar.js';
+import Feed from './Feed.js';
+import BottomBar from './BottomBar.js'
 
-const NavBar = () => (
-  <div className="navbar">
-    <h3>Task Manager</h3>
-    <Link to="/">Current Tasks</Link>
-    <Link to="/completed">Completed Tasks</Link>
-  </div>
-);
 
-const Template = (props) => (
-  <div>
-    <NavBar />
-    <p className="page-info">
-      {props.title}:
-    </p>
-    <ul className={props.status}>
-        <li>Task 1</li>
-        <li>Task 2</li>
-        <li>Task 3</li>
-    </ul>
-  </div>
-);
 
-const CurrentTasks = () => (
-  <Template title="Current Tasks" status="Current"/>
-);
-
-const CompletedTasks = () => (
-  <Template title="Completed Tasks" status="Completed"/>
-);
-
-class App extends Component {
-  render() {
+function App () {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={CurrentTasks}/>
-          <Route path="/completed" component={CompletedTasks}/>
-        </div>
-      </BrowserRouter>
+     <div className="App">
+       <NavBar />
+       <Feed />
+       <BottomBar />
+     </div> 
     );
-  }
 }
 
 export default App;
